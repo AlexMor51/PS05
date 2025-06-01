34dd9f54e84a56729a69074194f72f279041bf48
AlexMor51 <alexigmor@gmail.com>
2025-06-01 07:52:02 +0300

    add files lightpars

---
+++ lightpars/lightpars/spiders/lpars.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class LparsSpider(scrapy.Spider):
+    name = "lpars"
+    allowed_domains = ["divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        lights = response.css('div.lcSMD')
+        for light in lights:
+            yield  {
+                'name' :  light.css('div.lsooF span::text').getall()[0],
+                'price': light.css('div.lsooF span::text').getall()[1],
+                'url' : light.css('a::attr(href)').get()
+
+            }
